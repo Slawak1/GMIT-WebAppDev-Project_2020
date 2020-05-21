59756179368ab775e3b01b16e44b81521fcad874
slawak1 <sowa.slawomir@gmail.com>
2020-05-21 16:06:27 +0100

    update

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,60 +1,27 @@
-
-
-/*Dictionary stored for registered users*/
-
-
-
-var db = openDatabase("users", "3.30.1", "users", 200000);
-
-
-
-
-/* Method to validate entered password*/
-function pass_validate() {
 	
-	var pass1 = document.getElementById('password').value;
-	var pass2 = document.getElementById('confirm_password').value;
-	var email = document.getElementById('email').value;
-	
-	
-	  if (pass1 == pass2 && pass1.length > 6) {
-		document.getElementById('message').style.color = 'green';
-		document.getElementById('message').innerHTML = 'matching';
-	  } else {
+function myLogin() {
+	var email = "admin@admin.com";
+	var psw = "password";
+
+	var _email = document.getElementById("email").value;
+	var _pass = document.getElementById("password").value;
+
+	if (_email === email && _pass === psw) {
+		
+		window.open = 'main.html';
+
+		
+	} else if (_email === "" || _pass === "") {
 		document.getElementById('message').style.color = 'red';
-		document.getElementById('message').innerHTML = 'not matching';
-	  }
+		document.getElementById('message').innerHTML = 'Empty Values Detected';
 	
-	return email, pass1, pass2
-	
-	
-	}
-	
-	
-function save_user() {
-		
-	var pass1 = document.getElementById('password').value;
-	var pass2 = document.getElementById('confirm_password').value;
-	var email = document.getElementById('email').value;
-	
-	
-	if (pass1 == pass2 && pass1.length > 6) {
-		
-		
-		// Writing registered data into txt file to store
-		db.transaction(function (tx) {   
-		   tx.executeSql("INSERT INTO users (email,pass) VALUES('costam','222');"); 
-		});
-		alert("Registered Succesfuly");
 	}
 	else {
-
-		alert("Some Errors Occured");
+		document.getElementById('message').style.color = 'red';
+		document.getElementById('message').innerHTML = 'Wrong Email or Password';
 	}
-
-}
 	
-/* Function to load registration form and insert it into div id= rightn-box  */
-	function myReg() {
-		document.getElementById("right-box").innerHTML='<object type="text/html" data="register.html" width="800px" height="700px" ></object>'; 
 }
+
+
+
